c863aa8c6f7fa7be222c35ead1a94f3a9735fcc6
SzHubi <SzaHub608@hengersor.hu>
2025-09-11 13:36:29 +0200

    2ora

---
+++ proba/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(cors());
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    port: 3307,
+    password: '',
+    database: 'kozutak'
+}); 
+
+app.get('/', (req, res) => {
+    res.send('Fut a backend!')
+});
+app.get('/km', (req, res) => {
+    const sql = "SELECT * FROM kozutak.megyek";
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.get('/kh', (req, res) => {
+    const sql = "SELECT * FROM kozutak.kozutak_hossza";
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.get('/kr', (req, res) => {
+    const sql = "SELECT * FROM kozutak.regiok";
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+}); 
+
+
+app.delete('/torles/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = "DELETE FROM kozutak.megyek WHERE id = ?";  
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+            console.log("Sikeres törlés");
+        }
+    });
+});
