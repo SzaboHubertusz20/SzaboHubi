370c902f7f0c6e8215739ad44dd5b0c578da46d4
SzHubi <115551721+SzaboHubertusz20@users.noreply.github.com>
2025-09-24 15:47:41 +0200

    server

---
+++ Backend/js.server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(cors());
+const port = 3000;  
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const db = mysql.createConnection({
+    user:"root",
+    host:"localhost",
+    port:3307,
+    password:"",
+    database:""
+}); 
+
+app.get('/', (req, res) => {
+    res.send('Fut a backend!')
+});
